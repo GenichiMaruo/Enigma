4f8c69726fd7103bc141c8103203928d9c7b78ea
GenichiMaruo <85728541+GenichiMaruo@users.noreply.github.com>
2022-01-23 20:09:24 +0900

    rotorの作成プログラムを追加

---
+++ setrotor.py
@@ -0,0 +1,19 @@
+import random
+
+with open("data/base.txt",'r') as f:
+    basearray = [s.strip() for s in f.readlines()]
+
+cnt = input("count :")
+seed = input("seed  :")
+
+outputlen = []
+random.seed(int(seed))
+for i in range(int(cnt)):
+    outputlen.extend(random.sample(basearray,len(basearray)))
+    outputlen.append("#")
+for i in range(len(outputlen)):
+    outputlen[i] += "\n"
+
+out_file_name = "rotors.txt"
+with open(out_file_name,'w') as out_file:
+    out_file.writelines(outputlen)
